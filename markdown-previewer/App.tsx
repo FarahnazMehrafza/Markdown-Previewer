ed4a7102fcb7d04426e2f4c9864955f7af16c3f8
FIRST_NAME LAST_NAME <farahmehr1@gmail.com>
2024-03-07 21:40:40 -0500

    completd

---
+++ markdown-previewer/App.tsx
@@ -0,0 +1,33 @@
+import  { useState } from 'react';
+import Header from './src/components/Header';
+import MarkdownGuide from './src/components/MarkdownGuide';
+import MarkdownInput from './src/components/MarkdownInput';
+import MarkdownOutput from './src/components/MarkdownOutput';
+import  { ChangeEvent } from 'react';
+
+
+const App = () => {
+  const [markdown, setMarkdown] = useState<string>('# Hello World');
+  const [showGuide, setShowGuide] = useState<boolean>(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) =>  {
+    setMarkdown(event.target.value);
+  };
+
+  const onToggleGuide = () => {
+    setShowGuide(!showGuide);
+  };
+
+  return (
+    <div>
+      <Header onToggleGuide={onToggleGuide} />
+      {showGuide && <MarkdownGuide />}
+     
+      <MarkdownInput value={markdown}  onChange={handleChange}   />
+        <MarkdownOutput markdown={markdown} />
+     
+    </div>
+  );
+};
+
+export default App;
